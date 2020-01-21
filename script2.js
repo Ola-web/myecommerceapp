d5b163a5a6fd30f79b7c1b3b453561ef5b5ab471
Uthman Abdulazeez <olabounce36@gmail.com>
2020-01-20 17:27:18 -0800

    Initial commit

---
+++ script2.js
@@ -0,0 +1,23 @@
+
+    let openMenu = () => {
+    document.getElementById("side-menu").
+    style.display = "block";
+    document.getElementById("menu-btn").
+    style.display = "none";
+    document.getElementById("close-btn").
+    style.display = "block";
+}
+
+let closeMenu = () => {
+    document.getElementById("side-menu").
+    style.display = "none";
+    document.getElementById("menu-btn").
+    style.display = "block";
+    document.getElementById("close-btn").
+    style.display = "none";
+}
+   
+
+
+
+
